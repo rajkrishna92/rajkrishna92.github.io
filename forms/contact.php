2acb565ef1653d2c84293380e550c039d9018252
rajkrishna <rajkrishna.math@gmail.com>
2025-02-25 00:54:20 +0530

    --

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,16 @@
-<?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'rajkrishna.math@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+<!-- Replace {yourFormId} with your actual Formspree form ID -->
+<form action="https://formspree.io/f/mbldvdwq" method="POST">
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required>
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="_replyto" required>
+  
+  <label for="subject">Subject:</label>
+  <input type="text" id="subject" name="subject">
+  
+  <label for="message">Message:</label>
+  <textarea id="message" name="message" required></textarea>
+  
+  <button type="submit">Send</button>
+</form>
